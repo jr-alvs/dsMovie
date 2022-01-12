c4c9d41c38833cf875cc5e6f63a0b7e2c8e0fb2e
Junior Alves <helio.alvs@gmail.com>
2022-01-12 16:33:55 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as GithubIcon} from 'assets/img/githubIcon.svg';
+import './styles.css'
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/jr-alvs">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/jr-alvs</p>
+            </div>
+          </a>
+        </div>        
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
